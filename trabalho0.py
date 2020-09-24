3fb566dc898e96264500a3139fabcd39b0b417c4
Bruno Richau <19270033+Brunomri@users.noreply.github.com>
2020-09-24 13:22:51 -0300

    Adicionar imagens de exemplo e código para exibir uma imagem.

--- trabalho0.py
+++ trabalho0.py
@@ -1,5 +1,14 @@
 import cv2
 import skimage
+import sys
 
 print(cv2.__version__)
 print(skimage.__version__)
+
+img = cv2.imread("city.png")
+
+if img is None:
+    sys.exit("Could not read the image.")
+
+cv2.imshow("Display window", img)
+k = cv2.waitKey(0)
