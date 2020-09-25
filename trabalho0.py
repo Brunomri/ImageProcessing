77130459f9d6b62ef7705daa8e41d26fda952ad5
Bruno Richau <19270033+Brunomri@users.noreply.github.com>
2020-09-25 14:33:58 -0300

    Modificar escala de intensidade dos pixels.

--- trabalho0.py
+++ trabalho0.py
@@ -1,9 +1,11 @@
 # Processamento de imagens
 # Trabalho 0
 
-# Importar OpenCV e scikit-image
+# Importar OpenCV, scikit-image, Numpy e Matplotlib
 import cv2 as cv
 import skimage
+import numpy as np
+from matplotlib import pyplot as plt
 import sys
 
 print(cv.__version__)
@@ -11,30 +13,57 @@ print(skimage.__version__)
 
 # 1.1 Transformacao de intensidade
 # a) Imagem original
-img = cv.imread("city.png")
+img = cv.imread("city.png", 0)
 
 if img is None:
     sys.exit("Could not read the image.")
 
 cv.imshow("Original", img)
-k = cv.waitKey(0)
+k = cv.waitKey(0) & 0xFF
+
+# cv.destroyWindow("Original")
 
 # b) Negativo da imagem
 imgNeg = cv.bitwise_not(img)
 cv.imshow("Negativo", imgNeg)
-k = cv.waitKey(0)
+k = cv.waitKey(0) & 0xFF
 
 if k == ord("s"):
-    cv.imwrite("city1_1.png", imgNeg)
+    cv.imwrite("city1_1b.png", imgNeg)
+
+cv.destroyWindow("Negativo")
 
 # c) Espelhamento vertical
 imgFlip = cv.flip(img,0)
 cv.imshow("Espelhamento vertical", imgFlip)
-k = cv.waitKey(0)
+k = cv.waitKey(0) & 0xFF
 
 if k == ord("s"):
-    cv.imwrite("city1_2.png", imgFlip)
+    cv.imwrite("city1_1c.png", imgFlip)
+
+cv.destroyWindow("Espelhamento vertical")
 
 # d) Imagem transformada
+print(img[0,0])
+print(img.shape)
+
+rows,cols = img.shape
+g = img
+
+for i in range(rows):
+    for j in range(cols):
+        f = img[i,j]
+        v = ((100*f)/255)+100
+        g.itemset((i,j),v)
+
+cv.imshow("Imagem transformada", g)
+k = cv.waitKey(0) & 0xFF
+
+if k == ord("s"):
+    cv.imwrite("city1_1d.png", g)
+
+cv.destroyWindow("Imagem transformada")
 
 # e) Linhas pares invertidas
+
+cv.destroyAllWindows()
