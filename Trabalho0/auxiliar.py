fb602678b853e488f0826e7b751effaa0dd049c9
Bruno Richau <19270033+Brunomri@users.noreply.github.com>
2020-10-01 23:23:50 -0300

    Criar função para realizar a correção gamma.

--- Trabalho0/auxiliar.py
+++ Trabalho0/auxiliar.py
@@ -1,6 +1,7 @@
 # Funcoes auxiliares
 
 import cv2 as cv
+import numpy as np
 
 # exibir: exibe a imagem em uma janela e a salva em um arquivo se o usuario entrar "s", senão
 # apenas continua a execucao
@@ -12,3 +13,13 @@ def exibir(janela,img,saida):
         cv.imwrite(saida, img)
 
     cv.destroyWindow(janela)
+
+# correcaoGamma: corrige o brilho da imagem de acordo com a power law transform
+def correcaoGamma(img, gamma=1.0):
+    invGamma = 1.0 / gamma
+    # Utiliza uma tabela para mapear cada pixel e seu valor corrigido
+    # permitindo execucao em tempo O(1)
+    tabela = np.array([((i/255.0) ** invGamma) * 255
+                       for i in np.arange(0,256)]).astype("uint8")
+
+    return cv.LUT(img,tabela)
