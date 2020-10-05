a63c997260f1cba29f97d21f587bdd37278685d5
Bruno Richau <19270033+Brunomri@users.noreply.github.com>
2020-10-05 00:11:50 -0300

    Combinação de 2 imagens funcionando.

--- Trabalho0/modulo5.py
+++ Trabalho0/modulo5.py
@@ -6,4 +6,28 @@ import cv2 as cv
 import numpy as np
 import sys, auxiliar as a
 
-def questao5(img):
+def questao5(img, img2):
+
+    # a) Imagem A
+    a.exibir("Original A",img,"original5a.png")
+
+    # b) Imagem B
+    a.exibir("Original B",img2,"original5b.png")
+
+    # c) Combinar 0.2*A + 0.8*B
+    alpha = 0.2
+    beta = (1 - alpha)
+    res = cv.addWeighted(img, alpha, img2, beta, 0.0)
+    a.exibir("0.2*A + 0.8*B", res, "comb1_5c")
+
+    # d) Combinar 0.5*A + 0.5*B
+    alpha = 0.5
+    beta = (1 - alpha)
+    res = cv.addWeighted(img, alpha, img2, beta, 0.0)
+    a.exibir("0.5*A + 0.5*B", res, "comb1_5d")
+
+    # e) Combinar 0.8*A + 0.2*B
+    alpha = 0.8
+    beta = (1 - alpha)
+    res = cv.addWeighted(img, alpha, img2, beta, 0.0)
+    a.exibir("0.8*A + 0.2*B", res, "comb1_5e")
